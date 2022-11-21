e70fa359bb511a72101e17da80a61f574eb44291
gangfv <ganggstand@gmail.com>
2022-11-21 19:31:10 +0300

    good job

---
+++ main.py
@@ -0,0 +1,48 @@
+import re
+from itertools import zip_longest
+from transliterate import translit
+
+FILE_NAME = 'data_fio_number_sepped.txt'
+
+
+def read_file(file: str):
+    with open(file, encoding='utf-8') as f:
+        return (re.sub("\n", "", i).split(';') for i in f.readlines())
+
+
+def translit_en(list_ru):
+    return [translit(i, 'ru', True) for i in list_ru]
+
+
+def num_read(file: str):
+    return [i[1] for i in read_file(file)]
+
+
+def fio_read(file: str):
+    return [i[0] for i in read_file(file)]
+
+
+def num_ref(file: str):
+    num = []
+    for index, i in enumerate(num_read(file)):
+        split = re.split('\(|\)', i)
+        if len(split[1]) > 3:
+            split[1] = split[1][1:]
+        elif len(split[1]) < 3:
+            split[1] = f"9{split[1]}"
+        num.append(f"{split[0]}({split[1]}){split[2]}")
+    return num
+
+
+def fix_list(file: str):
+    return [*zip_longest(fio_read(file), num_ref(file))]
+
+
+result = 0
+for i in fix_list(FILE_NAME):
+    result += 1
+    for fio in fio_read(FILE_NAME):
+        with open(f"{translit(fio, 'ru', True)}.txt", "w", encoding='utf-8') as file:
+            file.write("{};{}".format(*i))
+
+print(f"Cгенерировано {result}")
